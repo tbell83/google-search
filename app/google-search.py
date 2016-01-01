a857abd720fb77aafb95e7a7c56e431167e2fdad
tbell83 <tbell@tombellnj.com>
2016-01-01 07:43:00 -0500

    pushing for posterity

--- app/google-search.py
+++ app/google-search.py
@@ -17,7 +17,7 @@ def googleTextSearch(query):
             search_results = google.search(query+' j')
 
         if len(search_results) > 0:
-            return search_results[0]
+            return search_results
         else:
             return False
 
@@ -27,11 +27,19 @@ def googleTextSearch(query):
 class Search(Resource):
     def get(self):
         token = request.args.get('token')
+        print token
+
         if token == slack_token:
+            print 'Token checks out'
             query = request.args.get('text')
+            print 'Query is {0}'.format(query)
             result = googleTextSearch(query)
-            if result:
-                attachment = {"attachments":[{"fallback":query,"pretext":query,"color":"#FFF","fields": [{"title":query,"value":'{0}\n{1}'.format(result.description, result.link)}],"image_url": url}]}
+            print result[0].description
+            print result[0].link
+            if result is not None:
+                print 'Getting some info...'
+                print '{0} {1}'.format(result[0].description, result[0].link)
+                attachment = {"attachments":[{"fallback":query,"pretext":query,"color":"#FFF","fields": [{"title":query,"value":'{0}\n{1}'.format(result[0].description, result[0].link)}],"image_url": url}]}
                 print "\n\n\n{0}\n{1}\n\n\n".format(url, attachment)
                 data = json.dumps(attachment)
                 post = requests.post(response_url, data=data)
